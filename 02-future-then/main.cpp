53793917e4f8a4db0a200d607363e49068bf91b5
Yehezkel Bernat <YehezkelShB@gmail.com>
2019-04-22 12:08:47 +0300

    Add `future-then` project (copy of `future-get`)

---
+++ 02-future-then/main.cpp
@@ -0,0 +1,15 @@
+#include "utils.h"
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) return EXIT_FAILURE;
+
+    auto f1 = readLines(argv[1]);
+    auto f2 = countLines(f1.get());
+
+    // ...
+
+    std::cout << f2.get() << '\n';
+}
---
+++ 02-future-then/utils.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+#include <future>
+#include <string>
+#include <vector>
+
+std::future<std::vector<std::string>> readLines(std::string path);
+std::future<uint64_t> countLines(std::vector<std::string> paths);
